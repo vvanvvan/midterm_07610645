9f35211d22d61ff9352b47b40e91154540a2556a
Saralee Jaipanya <jaipanya_s@silpakorn.edu>
2022-01-16 12:03:11 +0700

    Initial Commit

---
+++ lib/pig.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: avoid_print
+import 'main.dart';
+
+int? pigWeight(var girth, var length){
+ var gi = girth;
+ var le = length;
+ var weight = girth * girth * length * 69.3;
+
+ var total = weight;
+ return total;
+
+
+}
